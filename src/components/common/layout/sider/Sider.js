09b20fe27ed359d1caf1e90d6c1649c05e4d045b
longnv <longnvse@gmail.com>
2022-10-19 16:52:44 +0700

    update sider

--- src/components/common/layout/sider/Sider.js
+++ src/components/common/layout/sider/Sider.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import {Layout} from "antd";
+import React, {useState} from 'react';
+import {Layout, Menu} from "antd";
 import logo from "../../../../images/logo.png";
 
 const {Sider} = Layout
 
 function CommonSider({children}) {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const items = [
+        {
+            label: "Dự án",
+            key: "project"
+        },
+    ]
+
     return (
         <Sider
             width={250}
             theme={"light"}
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
         >
             <div>
                 <img src={logo} alt="logo" className={"app__header--logo"}/>
             </div>
             <div>
                 {children}
+                <Menu
+                    mode="inline"
+                    items={items}
+                />
             </div>
         </Sider>
     );
