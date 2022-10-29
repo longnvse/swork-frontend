1af00005c38dae44449170e977494ad8c9fdf0ac
longnv <longnvse@gmail.com>
2022-10-29 19:25:43 +0700

    update header

--- src/components/common/layout/header/Header.js
+++ src/components/common/layout/header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Layout} from "antd";
+import {Col, Layout} from "antd";
+import ProfileIcon from "./profile";
 
 const {Header} = Layout
 
-function CommonHeader({children}) {
+function CommonHeader() {
     return (
-        <Header className={"bg-orange-600"}>
-            {children}
+        <Header
+            className={"flex flex-row justify-between items-center"}
+            style={{
+                background: '#FFFFFFFF',
+                padding: 0
+            }}>
+            <Col>
+                <div className={"text__header pl-4"}>Danh sách công ty</div>
+            </Col>
+            <Col style={{
+                paddingRight: 4
+            }}>
+                <ProfileIcon/>
+            </Col>
         </Header>
     );
 }
