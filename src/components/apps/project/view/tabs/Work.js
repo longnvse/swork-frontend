93fe01863d68cb7586513d2e97dfd0a495350c0b
longnv <longnvse@gmail.com>
2022-12-08 22:17:11 +0700

    update work delete

--- src/components/apps/project/view/tabs/Work.js
+++ src/components/apps/project/view/tabs/Work.js
@@ -2,7 +2,7 @@ import {Button, Col, message, Popconfirm, Progress, Row, Table,} from "antd";
 import React, {useEffect, useState} from "react";
 import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 import ButtonDrawer from "../../../../common/button/ButtonDrawer";
-import {ADD, INACTIVE, UPDATE} from "../../../../common/Constant";
+import {ADD, DENIED, UPDATE} from "../../../../common/Constant";
 import WorkForm from "../../../work/form";
 import {deleteWork, getWorkPages} from "../../../../../api/work";
 import {columnsWork} from "../../../work/common/columns";
@@ -38,6 +38,7 @@ function ProjectViewWork({projectId, phaseId}) {
     const mapData = (data) => {
         if (data?.length <= 0) return [];
         return data?.map((item) => {
+            console.log(item.status);
             return {
                 key: item.id,
                 ...item,
@@ -67,13 +68,13 @@ function ProjectViewWork({projectId, phaseId}) {
                             <WorkForm workId={item?.id}/>
                         </ButtonDrawer>
                         <Popconfirm
-                            disabled={item.status !== INACTIVE}
+                            disabled={item.status !== DENIED}
                             title={"Chắc chắn chứ!"}
                             onConfirm={() => onConfirmDelete(item.id)}
                         >
                             <Button
                                 type={"link"}
-                                disabled={item.status !== INACTIVE}
+                                disabled={item.status !== DENIED}
                                 icon={<DeleteOutlined/>}
                             />
                         </Popconfirm>
