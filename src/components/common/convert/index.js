af4da3dd107634d46bdc04cea410f4ca7d7ef0c5
longnv <longnv@gmail.com>
2022-12-26 14:45:15 +0700

    fix convert money

--- src/components/common/convert/index.js
+++ src/components/common/convert/index.js
@@ -1,15 +1,14 @@
 import {formatMoney} from "./format";
 
 export function convertMoney(labelValue) {
-    if (!labelValue) {
-        return "0";
-    }
     // Nine Zeroes for Billions
-    return Math.abs(Number(labelValue)) >= 1.0e9
-        ? Number(labelValue) / 1.0e9 + " tỷ"
-        : // Six Zeroes for Millions
-        Math.abs(Number(labelValue)) >= 1.0e6
-            ? Number(labelValue) / 1.0e6 + " triệu"
-            : // Three Zeroes for Thousands
-            formatMoney(labelValue);
+    return Math.abs(Number(labelValue)) >= 1.0e+9
+
+        ? (Math.abs(Number(labelValue)) / 1.0e+9) + " tỷ"
+        // Six Zeroes for Millions
+        : Math.abs(Number(labelValue)) >= 1.0e+6
+
+            ? (Math.abs(Number(labelValue)) / 1.0e+6) + " triệu"
+            // Three Zeroes for Thousands
+            : formatMoney(labelValue)
 }
