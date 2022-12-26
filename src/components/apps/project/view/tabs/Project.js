a8115e93fe990d88ac5c4955277276e0ae6aed2c
longnv <longnvse@gmail.com>
2022-12-26 11:56:18 +0700

    fix comment

--- src/components/apps/project/view/tabs/Project.js
+++ src/components/apps/project/view/tabs/Project.js
@@ -1,31 +1,37 @@
-import {Col, Row} from "antd";
+import { Col, Row } from "antd";
 import React from "react";
 import ProjectViewGeneral from "./elements/General";
 import ProjectViewSummary from "./elements/Summary";
 import DashboardStatusWork from "../../dashboard/status-work";
 import CommentList from "../../../../common/comment/list";
-import {CLASS_PK_NAME} from "../../../../common/Constant";
+import { CLASS_PK_NAME } from "../../../../common/Constant";
 import DashboardProgressPhase from "../../dashboard/phase-progress";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const ProjectViewDetail = ({data, teamData, phaseData}) => {
+const ProjectViewDetail = ({ data, teamData, phaseData }) => {
     return (
         <Row gutter={12}>
-            <Col
-                span={7}
-            >
+            <Col span={7}>
                 <Row
-                    className={"rounded-[8px] border-solid border-[1px] border-[#ccc] w-full h-fit"}>
-                    <ProjectViewGeneral data={data}/>
+                    className={
+                        "rounded-[8px] border-solid border-[1px] border-[#ccc] w-full h-fit"
+                    }
+                >
+                    <ProjectViewGeneral data={data} />
                 </Row>
                 <Row
-                    className={"rounded-[8px] border-solid border-[1px] border-[#ccc] mt-1.5 p-6 w-full"}
+                    className={
+                        "rounded-[8px] border-solid border-[1px] border-[#ccc] mt-1.5 p-6 w-full"
+                    }
                 >
                     <Row className={"w-full"}>
                         <strong>Thảo luận</strong>
                     </Row>
-                    <Row className={"w-full h-[400px] overflow-auto"}>
-                        <CommentList classPkId={data.id} classPkName={CLASS_PK_NAME.PROJECT}/>
+                    <Row className={"h-[400px] overflow-x-hidden"}>
+                        <CommentList
+                            classPkId={data.id}
+                            classPkName={CLASS_PK_NAME.PROJECT}
+                        />
                     </Row>
                 </Row>
             </Col>
@@ -38,42 +44,57 @@ const ProjectViewDetail = ({data, teamData, phaseData}) => {
                     />
                 </Row>
                 <Row className={"mt-1.5 h-fit"}>
-                    <Col className={"rounded-[8px] border-solid border-[1px] border-[#ccc] p-6 w-full"}>
+                    <Col
+                        className={
+                            "rounded-[8px] border-solid border-[1px] border-[#ccc] p-6 w-full"
+                        }
+                    >
                         <div>
-                        <span>
-                            <strong>Giai đoạn</strong>
-                        </span>
-                            <Link to={`/project/view/${data.id}?tab=phase`}
-                                  style={{fontSize: 12, paddingLeft: 7}}>
+                            <span>
+                                <strong>Giai đoạn</strong>
+                            </span>
+                            <Link
+                                to={`/project/view/${data.id}?tab=phase`}
+                                style={{ fontSize: 12, paddingLeft: 7 }}
+                            >
                                 <i>Chi tiết</i>
                             </Link>
                         </div>
-                        <div style={{
-                            paddingTop: 15,
-                            width: '100%'
-                        }}>
-                            <DashboardProgressPhase projectId={data.id}/>
+                        <div
+                            style={{
+                                paddingTop: 15,
+                                width: "100%",
+                            }}
+                        >
+                            <DashboardProgressPhase projectId={data.id} />
                         </div>
                     </Col>
                 </Row>
                 <Row className={"mt-1.5"}>
                     <Col
                         span={10}
-                        className={"rounded-[8px] border-solid border-[1px] border-[#ccc] p-6 w-full"}>
+                        className={
+                            "rounded-[8px] border-solid border-[1px] border-[#ccc] p-6 w-full"
+                        }
+                    >
                         <div>
-                        <span>
-                            <strong>Công việc</strong>
-                        </span>
-                            <Link to={`/project/view/${data.id}?tab=work`}
-                                  style={{fontSize: 12, paddingLeft: 7}}>
+                            <span>
+                                <strong>Công việc</strong>
+                            </span>
+                            <Link
+                                to={`/project/view/${data.id}?tab=work`}
+                                style={{ fontSize: 12, paddingLeft: 7 }}
+                            >
                                 <i>Chi tiết</i>
                             </Link>
                         </div>
-                        <div style={{
-                            paddingTop: 15,
-                            width: '100%'
-                        }}>
-                            <DashboardStatusWork projectId={data?.id}/>
+                        <div
+                            style={{
+                                paddingTop: 15,
+                                width: "100%",
+                            }}
+                        >
+                            <DashboardStatusWork projectId={data?.id} />
                         </div>
                     </Col>
                 </Row>
--- src/components/common/comment/list.js
+++ src/components/common/comment/list.js
@@ -1,35 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import {getCommentPages} from "../../../api/comment";
-import {message_error} from "../Constant";
+import React, { useEffect, useState } from "react";
+import { getCommentPages } from "../../../api/comment";
+import { message_error } from "../Constant";
 import SWComment from "./index";
-import {Comment} from "@ant-design/compatible";
-import {Avatar} from "antd";
+import { Comment } from "@ant-design/compatible";
+import { Avatar } from "antd";
 import CommentForm from "./form";
-import {UserOutlined} from "@ant-design/icons";
-import {useSelector} from "react-redux";
+import { UserOutlined } from "@ant-design/icons";
+import { useSelector } from "react-redux";
 
-const CommentList = ({classPkId, classPkName}) => {
+const CommentList = ({ classPkId, classPkName }) => {
     const [data, setData] = useState([]);
-    const {reload, avatar} = useSelector(state => state.commonReducer);
+    const { reload, avatar } = useSelector((state) => state.commonReducer);
     useEffect(() => {
         if ((classPkId && classPkName) || reload) {
-            getCommentPages(classPkId, classPkName).then(res => {
-                setData(res.data?.items || []);
-            }).catch(message_error)
+            getCommentPages(classPkId, classPkName)
+                .then((res) => {
+                    setData(res.data?.items || []);
+                })
+                .catch(message_error);
         }
     }, [classPkId, classPkName, reload]);
-
 
     return (
         <>
             <Comment
                 className={"w-full"}
-                avatar={<Avatar src={avatar} icon={<UserOutlined/>}/>}
+                avatar={<Avatar src={avatar} icon={<UserOutlined />} />}
                 content={
-                    <CommentForm classPkId={classPkId} classPkName={classPkName}/>
+                    <CommentForm
+                        classPkId={classPkId}
+                        classPkName={classPkName}
+                    />
                 }
             />
-            {data.map(value => <SWComment {...value} classPkId={classPkId} classPkName={classPkName}/>)}
+            {data.map((value) => (
+                <SWComment
+                    {...value}
+                    classPkId={classPkId}
+                    classPkName={classPkName}
+                />
+            ))}
         </>
     );
 };
